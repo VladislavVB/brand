2d0c048eb39ea404d9824cfe383efc35ace7aeb8
VladislavVB <burdukovskij.vlad@mail.ru>
2021-01-14 19:28:41 +0300

    slide fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 const buttomMobileMenu = document.querySelector('.firstb-menu');
 const navigationMenu = document.querySelector('.burger-menu');
 buttomMobileMenu.addEventListener('click', () => {
-    
+
     navigationMenu.classList.toggle('activebur');
 })
 //язык
@@ -12,10 +12,23 @@ buttomMobileMenuLang.addEventListener('click', () => {
     navigationMenuLang.classList.toggle('menu-lang-active');
 })
 //показать/скрыть новости
-let allNews = () => {
-    // document.querySelector('.form-img-success').classList.add('form-img-success-active');
-    // document.querySelector('.form-form').classList.add('form-form-none');
+
+const showAllCards = () => {
+    if (isOpen) {
+        document.querySelector('.news-all').innerHTML = 'Explore All'
+        hiddenCard.forEach(element => {
+            element.classList.add('hidden-card-sm');
+        });
+        isOpen = false;
+    } else {
+        document.querySelector('.news-all').innerHTML = 'Close'
+        hiddenCard.forEach(element => {
+            element.classList.remove('hidden-card-sm');
+        });
+        isOpen = true;
+    }
 }
+
 //"отправка формы"
 let sendSuccess = () => {
     document.querySelector('.form-img-success').classList.add('form-img-success-active');
