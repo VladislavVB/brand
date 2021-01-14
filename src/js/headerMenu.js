bc6f8e491c2312e925bd1fdbfff89d0139a4aa01
VladislavVB <burdukovskij.vlad@mail.ru>
2021-01-14 13:56:07 +0300

    menu

---
+++ src/js/headerMenu.js
@@ -0,0 +1,56 @@
+$(function() {
+    $(window).on("scroll", function() {
+
+        console.log($(window).scrollTop());
+
+        if($(window).scrollTop() > 906) {
+            $(".menu-balck").css("color", "#1e1e1e")
+            $(".menu-balck-svg").css("fill", "#1e1e1e")
+            $(".menu-balck-svg").css("stroke", "#1e1e1e")
+            $(".menu-balck-svg").css("viewBox", "#1e1e1e")
+            $(".balck-vector").css("fill", "#1e1e1e")
+            $(".balck-vector").css("stroke", "#1e1e1e")
+            $(".balck-vector").css("rect", "#1e1e1e")
+            $(".menu-secondb").css("opacity", "1")
+            $(".menu").addClass("menu-active");  
+        } else {
+            $(".menu-balck").css("color", "white")
+            $(".menu-balck-svg").css("fill", "white")
+            $(".menu-balck-svg").css("stroke", "white")
+            $(".balck-vector").css("fill", "white") 
+            $(".balck-vector").css("stroke", "white")
+            $(".menu-secondb").css("opacity", "0")
+            $(".menu").removeClass("menu-active");  
+        }
+    });
+
+    let toggle = false;
+
+    $(".menu-click").click(function() {
+        console.log($(".menu-active"));
+        if (!toggle){
+            $(".menu-balck").css("color", "#1e1e1e")
+            $(".menu-balck-svg").css("fill", "#1e1e1e")
+            $(".menu-balck-svg").css("stroke", "#1e1e1e")
+            $(".balck-vector").css("fill", "#1e1e1e") 
+            $(".balck-vector").css("stroke", "#1e1e1e")
+            $(".menu-secondb").css("opacity", "1")
+            $(".menu").removeClass("menu-active");  
+            toggle = true
+        } else {
+            $(".menu-balck").css("color", "white")
+            $(".menu-balck-svg").css("fill", "white")
+            $(".menu-balck-svg").css("stroke", "white")
+            $(".balck-vector").css("fill", "white") 
+            $(".balck-vector").css("stroke", "white")
+            $(".menu-secondb").css("opacity", "0")
+            $(".menu").removeClass("menu-active");  
+            toggle = false
+        }
+        
+        
+      });
+    
+
+});
+
--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,13 @@ const navigationMenuLang = document.querySelector('.menu-lang');
 buttomMobileMenuLang.addEventListener('click', () => {
     navigationMenuLang.classList.toggle('menu-lang-active');
 })
+//показать/скрыть новости
+let allNews = () => {
+    // document.querySelector('.form-img-success').classList.add('form-img-success-active');
+    // document.querySelector('.form-form').classList.add('form-form-none');
+}
+//"отправка формы"
+let sendSuccess = () => {
+    document.querySelector('.form-img-success').classList.add('form-img-success-active');
+    document.querySelector('.form-form').classList.add('form-form-none');
+}
