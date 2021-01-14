982801b6597baae2488ba3eb0e12ab99cbf8d6b8
VladislavVB <burdukovskij.vlad@mail.ru>
2021-01-14 11:17:16 +0300

    lang

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
+//меню
 const buttomMobileMenu = document.querySelector('.firstb-menu');
 const navigationMenu = document.querySelector('.burger-menu');
 buttomMobileMenu.addEventListener('click', () => {
     navigationMenu.classList.toggle('activebur');
+})
+//язык
+const buttomMobileMenuLang = document.querySelector('.firstb-lang');
+const navigationMenuLang = document.querySelector('.menu-lang');
+buttomMobileMenuLang.addEventListener('click', () => {
+    navigationMenuLang.classList.toggle('menu-lang-active');
 })
