99ce3a6c472e70bd4b9c658b9f94bb3f6d4e3a0e
VladislavVB <burdukovskij.vlad@mail.ru>
2021-01-18 20:20:31 +0300

    fix 50%

--- src/js/main.js
+++ src/js/main.js
@@ -66,3 +66,18 @@ let sendSuccess = () => {
     document.querySelector('.form-img-success').classList.add('form-img-success-active');
     document.querySelector('.form-form').classList.add('form-form-none');
 }
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
