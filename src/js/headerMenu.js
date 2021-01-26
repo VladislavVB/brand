7c7d00ed7c087cb4a9263e677f6ccf2fecb1c166
VladislavVB <burdukovskij.vlad@mail.ru>
2021-01-26 13:56:15 +0300

    cheackpoint

--- src/js/headerMenu.js
+++ src/js/headerMenu.js
@@ -1,6 +1,6 @@
 let scrollToggle = false
 let switchMenuColor = (height) => {
-    if(height > 906) {
+    if(height > 70) {
         document.querySelector('.menu').classList.add('body-menu-active')
     } else { 
         if(scrollToggle) {
@@ -34,73 +34,3 @@ $(function() {
        switchMenuColor($(window).scrollTop())
     });
 });
-
-// switchMenuColor()
-
-// let toggle = false;
-
-// let switchMenuColor = (height) => {
-//     if(height > 906) {
-//         $(".menu-balck").css("color", "#1e1e1e")
-//         $(".menu-balck-svg").css("fill", "#1e1e1e")
-//         $(".menu-balck-svg").css("stroke", "#1e1e1e")
-//         $(".menu-balck-svg").css("viewBox", "#1e1e1e")
-//         $(".balck-vector").css("fill", "#1e1e1e")
-//         $(".balck-vector").css("stroke", "#1e1e1e")
-//         $(".balck-vector").css("rect", "#1e1e1e")
-//         $(".menu-secondb").css("opacity", "1")
-//         $(".menu").addClass("menu-active");  
-       
-//     } else {
-       
-//         $(".menu-balck").css("color", "white")
-//         $(".menu-balck-svg").css("fill", "white")
-//         $(".menu-balck-svg").css("stroke", "white")
-//         $(".balck-vector").css("fill", "white") 
-//         $(".balck-vector").css("stroke", "white")
-//         $(".menu-secondb").css("opacity", "0")
-//         $(".menu").removeClass("menu-active");  
-//     }
-// }
-// switchMenuColor($(window).scrollTop())
-
-
-
-//     switchMenuColor = (height) => {
-//         if(height > 906) {
-//             document.querySelector('menu').classList.add('body-menu-active')
-//         }
-//     }
-    
-
-//     $(".menu-click").click(function() {
-//         console.log($(".menu-active"));
-//         if (!toggle){
-            
-            
-//             $(".menu-balck").css("color", "#1e1e1e")
-//             $(".menu-balck-svg").css("fill", "#1e1e1e")
-//             $(".menu-balck-svg").css("stroke", "#1e1e1e")
-//             $(".balck-vector").css("fill", "#1e1e1e") 
-//             $(".balck-vector").css("stroke", "#1e1e1e")
-//             $(".menu-secondb").css("opacity", "1")
-//             $(".menu").removeClass("menu-active");  
-//             toggle = true
-//         } else {
-//             $(".menu-balck-svg").css("stroke", "white")
-//             $(".balck-vector").css("fill", "white") 
-//             $(".balck-vector").css("stroke", "white")
-//             $(".menu-balck").css("color", "white")
-//             $(".menu-balck-svg").css("fill", "white")
-//             $(".menu-secondb").css("opacity", "0")
-//             $(".menu").removeClass("menu-active");  
-//             switchMenuColor($(window).scrollTop())
-//             toggle = false
-//         }
-        
-//       });
-    
-// });
-
-
-
