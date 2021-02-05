26b79c486f5ba767cd4ef2edec1f68553c00f880
VladislavVB <burdukovskij.vlad@mail.ru>
2021-02-05 20:37:08 +0300

    add video

--- src/js/mainJq.js
+++ src/js/mainJq.js
@@ -1,1 +1,16 @@
-
+$( document ).ready(function() {
+  var player;
+  $('.serm-video-play').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      // height: '360',
+      // width: '640',
+      videoId: 'cfn40rJaCd4',
+      events: {
+        'onReady': videoplay,
+      }
+    });
+  })
+  function videoplay(event) {
+    event.target.playVideo();
+  }
+});
