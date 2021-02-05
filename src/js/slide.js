4103724c5c78be361351dcf007756261d425a9c9
VladislavVB <burdukovskij.vlad@mail.ru>
2021-02-05 22:39:11 +0300

    add slider

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,12 +1,12 @@
-let swiperSponsors = new Swiper('.swiper-container', {
+let swiperSponsors = new Swiper('.swiper-container-sponsor', {
     slidesPerView: 4,
     spaceBetween: 30,
     pagination: {
-        el: '.swiper-pagination',
+        el: '.swiper-pagination-sponsor',
         type: 'fraction',
     },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next-sponsor',
+        prevEl: '.swiper-button-prev-sponsor',
     },
 });
