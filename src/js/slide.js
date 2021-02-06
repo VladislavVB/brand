63f0008eb0589090cee1ae9fc11d1717fdb308d6
VladislavVB <burdukovskij.vlad@mail.ru>
2021-02-06 18:04:49 +0300

    create ebay

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,12 +1,27 @@
-let swiperSponsors = new Swiper('.swiper-container-sponsor', {
+var swiperSponsors = new Swiper('.swiper-container-sponsor', {
     slidesPerView: 4,
     spaceBetween: 30,
     pagination: {
         el: '.swiper-pagination-sponsor',
         type: 'fraction',
     },
-    navigation: {
+    navigation: {   
         nextEl: '.swiper-button-next-sponsor',
         prevEl: '.swiper-button-prev-sponsor',
     },
 });
+
+// var swiperCustomers = new Swiper('.swiper-container-customers', {
+//     slidesPerView: 1,
+//     // simulateTouch: false,
+//     pagination: {
+//         el: '.swiper-pagination-customers',
+//         type: 'fraction',
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next-customers',
+//         prevEl: '.swiper-button-prev-customers',
+//     },
+// });
+
+
