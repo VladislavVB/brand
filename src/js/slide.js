d0f15cfd736765eae2f94e7f67e26853a720c104
VladislavVB <burdukovskij.vlad@mail.ru>
2021-02-06 20:59:42 +0300

    create request

--- src/js/slide.js
+++ src/js/slide.js
@@ -11,9 +11,21 @@ var swiperSponsors = new Swiper('.swiper-container-sponsor', {
     },
 });
 
+var swiperCustomers = new Swiper('.swiper-container-customers', {
+    slidesPerView: 1,
+    simulateTouch: false,
+    pagination: {
+        el: '.swiper-pagination-customers',
+        type: 'fraction',
+    },
+    navigation: {   
+        nextEl: '.swiper-button-next-customers',
+        prevEl: '.swiper-button-prev-customers',
+    },
+});
 // var swiperCustomers = new Swiper('.swiper-container-customers', {
 //     slidesPerView: 1,
-//     // simulateTouch: false,
+//     
 //     pagination: {
 //         el: '.swiper-pagination-customers',
 //         type: 'fraction',
